1b1b88b4908975ca62fccc4437b36fb3befc2045
Aziz Boudi <azicode4@gmail.com>
2015-11-08 13:43:53 -0800

    - Added pan gesture recognizer ( now you swipe and the counts goes up as you are swiping)
    - TODO:
    - swipe down decreases the count
    - activate both the pan gesture and the swipe

--- newStepper/ViewController.swift
+++ newStepper/ViewController.swift
@@ -15,18 +15,14 @@ import QuartzCore
 
 class ViewController: UIViewController {
 
+
   // the view underneath the number
 
   @IBOutlet weak var circleView: UIView!
 
-  struct Colors {
-    static let jogaGreen = UIColor(
-      red: 167/255.0,
-      green: 246/255.0,
-      blue: 67/255.0,
-      alpha: 1
-    )
-  }
+
+  var increment: Int = 1
+  var offset: CGFloat = 10
 
 
   @IBInspectable var min: Int = 0
@@ -54,14 +50,13 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     label.font = UIFont(name:"Futura-Medium", size: 44.0)
+
     setupSwipeGestures()
+    setupPanGestures()
 
     circleView.layer.cornerRadius = CGRectGetHeight(circleView.bounds) / 2.0
     circleView.layer.borderColor = UIColor.lightGrayColor().CGColor
-//    circleView.layer.borderWidth = 9.0
-//    circleView.layer.shadowRadius = 4
-//    circleView.layer.shadowOpacity = 0.5
-//    circleView.layer.shadowColor = UIColor.blueColor().CGColor
+
   }
 
   private func setupSwipeGestures() {
@@ -73,15 +68,34 @@ class ViewController: UIViewController {
 
     circleView.addGestureRecognizer(swipeUp)
     circleView.addGestureRecognizer(swipeDown)
+  }
+
+
+
+  private func setupPanGestures() {
+    let panGesture = UIPanGestureRecognizer(target: self, action: Selector("handleThePan:"))
+
+    circleView.addGestureRecognizer(panGesture)
+
+
+
+  }
+
+  func handleThePan(sender: UIPanGestureRecognizer) -> Void {
+
+    if sender.isUp(circleView) {
+
+
+    } else {
+
+    }
+
+    inc(increment)
 
 
   }
 
   func handleSwipes(sender:UISwipeGestureRecognizer) {
-    var increment: Int = 1
-    var offset: CGFloat = 10
-
-
     // up or down
     if sender.direction == .Down && score == 0  {
       increment = 0
@@ -107,15 +121,12 @@ class ViewController: UIViewController {
     inc(increment)
 
     UIView.animateWithDuration(0.2, animations: { _ in
-      self.labelYConstraint.constant = offset
+      self.labelYConstraint.constant = self.offset
       self.view.layoutIfNeeded()
 
-      //self.circleView.layer.shadowRadius = 4
       self.circleView.layer.shadowOpacity = 1
-      //self.circleView.layer.shadowColor = UIColor(red: 167/255.0, green: 246/255.0, blue: 67/255.0, alpha: 1).CGColor
       self.circleView.layer.backgroundColor = UIColor(red: 167/255.0, green: 246/255.0, blue: 67/255.0, alpha: 1).CGColor
       self.circleView.layer.shadowOffset = CGSize.zero
-      //self.label.textColor = UIColor.blueColor()
       }) { _ in
 
         UIView.animateWithDuration(0.2, animations: { _ in
@@ -126,9 +137,24 @@ class ViewController: UIViewController {
           self.circleView.layer.shadowColor = UIColor.clearColor().CGColor
           self.circleView.layer.backgroundColor = UIColor(red: 211/255.0, green: 211/255.0, blue: 211/255.0, alpha: 0.3).CGColor
 
-          //self.label.textColor = UIColor.blackColor()
         })
     }
   }
 }
+
+extension UIPanGestureRecognizer {
+
+  func isUp(circleView: UIView) -> Bool {
+    let velocity : CGPoint = velocityInView(circleView)
+    if velocity.y < 0 {
+      print("ex Gesture went up")
+      return false
+    } else {
+      print("ex Gesture went down")
+      return true
+    }
+  }
+}
+
+
 
